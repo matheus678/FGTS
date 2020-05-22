9013104fda3a6fda908bba686a447b9f37b25ef7
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-22 02:31:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Olá usuário, você precisa calcular seu FGTS.");
+
+            Console.WriteLine(" Pressione enter para continuar:");
+
+            Console.ReadKey();
+
+
+
+
+            Console.WriteLine("Informe o seu salário atual em reais:");
+
+            double salario = double.Parse(Console.ReadLine());
+
+
+
+            Console.Write("A parcela do seu FGTS é: ");
+
+            double resultado = (salario * 8 / 100);
+
+
+
+
+            Console.Write( resultado);
+
+            Console.WriteLine(" reais.");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
